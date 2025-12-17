88ee054ab00d36da98fbdd1ac99009787aa23fc1
Dean Howe <hideanhowe@gmail.com>
2025-12-17 16:51:25 +0000

    solution

--- index.js
+++ index.js
@@ -4,3 +4,34 @@
 // For example, given [1, 2, 3, 4, 5] and ['+', '*'], return the result of evaluating 1 + 2 * 3 + 4 * 5 from left-to-right ignoring standard order of operations.
 
 // Valid operators are +, -, *, /, and %.
+
+function evaluate(numbers, operators) {
+  console.log(numbers, operators);
+  let result = numbers[0];
+  let counter = 0;
+  for (let i = 1; i <= numbers.length - 1; i++) {
+    let op = operators[(i - 1) % operators.length];
+    // take the first and second
+    // console.log(numbers[i], numbers[i + 1])
+    // result = result operators[] numbers[i];
+    switch (op) {
+      case '+':
+        result = result + numbers[i];
+        break;
+      case '-':
+        result = result - numbers[i];
+        break;
+      case '*':
+        result = result * numbers[i];
+        break;
+      case '/':
+        result = result / numbers[i];
+        break;
+      case '%':
+        result = result % numbers[i];
+        break;
+    }
+  }
+
+  return result;
+}
